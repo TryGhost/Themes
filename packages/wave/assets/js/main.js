233d8da10bc4c3e1ddfb6d3d1f484ee63f47cd20
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2019-12-04 21:01:45 +0800

    Many changes

--- packages/wave/assets/js/main.js
+++ packages/wave/assets/js/main.js
@@ -12,11 +12,25 @@ document.addEventListener('lazyloaded', function (e) {
 
 $(function () {
   'use strict';
+  gallery();
   player();
   pagination();
   popup();
   subscribe();
 });
+
+function gallery() {
+  'use strict';
+  var images = document.querySelectorAll('.kg-gallery-image img');
+
+  images.forEach(function (image) {
+    var container = image.closest('.kg-gallery-image');
+    var width = image.attributes.width.value;
+    var height = image.attributes.height.value;
+    var ratio = width / height;
+    container.style.flex = ratio + ' 1 0%';
+  });
+}
 
 function player() {
   'use strict';
