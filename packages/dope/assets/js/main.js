4dd68f3ad887e6ef3cc6d13a0464c9ae7971c2a1
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2020-09-28 14:47:46 +0800

    Add prettier config file and format main js file

--- packages/dope/assets/js/main.js
+++ packages/dope/assets/js/main.js
@@ -1,7 +1,7 @@
-var html = $("html");
+var html = $('html');
 
 $(function () {
-    "use strict";
+    'use strict';
     tagFeed();
     loadMore();
     video();
@@ -11,25 +11,25 @@ $(function () {
     facebook();
 });
 
-document.addEventListener("lazyloaded", function (e) {
-    "use strict";
+document.addEventListener('lazyloaded', function (e) {
+    'use strict';
     var options = {
         disableParallax: /iPad|iPhone|iPod|Android/,
         disableVideo: /iPad|iPhone|iPod|Android/,
         speed: 0.1,
     };
 
-    if ($(e.target).closest(".post").hasClass("single-post")) {
-        $(e.target).parent().jarallax(options).addClass("initialized");
+    if ($(e.target).closest('.post').hasClass('single-post')) {
+        $(e.target).parent().jarallax(options).addClass('initialized');
     }
 });
 
 function tagFeed() {
-    "use strict";
-    $(".tag-feed").owlCarousel({
+    'use strict';
+    $('.tag-feed').owlCarousel({
         dots: false,
         nav: true,
-        navText: ["", ""],
+        navText: ['', ''],
         onInitialized: function () {
             fixHorizontalScroll();
         },
@@ -54,24 +54,24 @@ function tagFeed() {
 }
 
 function loadMore() {
-    "use strict";
-    var wrapper = $(".post-feed");
-    var button = $(".pagination");
+    'use strict';
+    var wrapper = $('.post-feed');
+    var button = $('.pagination');
     var content, link, page;
 
-    button.on("click", function () {
-        $.get($(this).attr("data-url"), function (data) {
-            content = $(data).find(".post-feed > *");
-            link = $(data).find(".pagination").attr("data-url");
-            page = $(data).find(".page-number").text();
+    button.on('click', function () {
+        $.get($(this).attr('data-url'), function (data) {
+            content = $(data).find('.post-feed > *');
+            link = $(data).find('.pagination').attr('data-url');
+            page = $(data).find('.page-number').text();
 
             wrapper.append(content);
 
-            button.attr("data-url", link);
-            button.find(".page-number").text(page);
+            button.attr('data-url', link);
+            button.find('.page-number').text(page);
 
             if (link) {
-                button.attr("data-url", link);
+                button.attr('data-url', link);
             } else {
                 button.remove();
             }
@@ -80,78 +80,78 @@ function loadMore() {
 }
 
 function video() {
-    "use strict";
-    $(".post-content").fitVids();
+    'use strict';
+    $('.post-content').fitVids();
 }
 
 function gallery() {
-    "use strict";
-    var images = document.querySelectorAll(".kg-gallery-image img");
+    'use strict';
+    var images = document.querySelectorAll('.kg-gallery-image img');
     images.forEach(function (image) {
-        var container = image.closest(".kg-gallery-image");
+        var container = image.closest('.kg-gallery-image');
         var width = image.attributes.width.value;
         var height = image.attributes.height.value;
         var ratio = width / height;
-        container.style.flex = ratio + " 1 0%";
+        container.style.flex = ratio + ' 1 0%';
     });
 }
 
 function author() {
-    "use strict";
-    $(".author-name").on("click", function () {
-        $(this).next(".author-social").toggleClass("enabled");
+    'use strict';
+    $('.author-name').on('click', function () {
+        $(this).next('.author-social').toggleClass('enabled');
     });
 }
 
 function offCanvas() {
-    "use strict";
-    var burger = jQuery(".burger");
-    var canvasClose = jQuery(".canvas-close");
+    'use strict';
+    var burger = jQuery('.burger');
+    var canvasClose = jQuery('.canvas-close');
 
-    burger.on("click", function () {
-        html.toggleClass("canvas-opened");
-        html.addClass("canvas-visible");
-        dimmer("open", "medium");
+    burger.on('click', function () {
+        html.toggleClass('canvas-opened');
+        html.addClass('canvas-visible');
+        dimmer('open', 'medium');
     });
 
-    canvasClose.on("click", function () {
-        if (html.hasClass("canvas-opened")) {
-            html.removeClass("canvas-opened");
-            dimmer("close", "medium");
+    canvasClose.on('click', function () {
+        if (html.hasClass('canvas-opened')) {
+            html.removeClass('canvas-opened');
+            dimmer('close', 'medium');
         }
     });
 
-    jQuery(".dimmer").on("click", function () {
-        if (html.hasClass("canvas-opened")) {
-            html.removeClass("canvas-opened");
-            dimmer("close", "medium");
+    jQuery('.dimmer').on('click', function () {
+        if (html.hasClass('canvas-opened')) {
+            html.removeClass('canvas-opened');
+            dimmer('close', 'medium');
         }
     });
 
     jQuery(document).keyup(function (e) {
-        if (e.keyCode == 27 && html.hasClass("canvas-opened")) {
-            html.removeClass("canvas-opened");
-            dimmer("close", "medium");
+        if (e.keyCode == 27 && html.hasClass('canvas-opened')) {
+            html.removeClass('canvas-opened');
+            dimmer('close', 'medium');
         }
     });
 }
 
 function facebook() {
-    "use strict";
-    if ($(".fb-page").attr("data-href") == "") {
-        $(".widget-facebook").remove();
+    'use strict';
+    if ($('.fb-page').attr('data-href') == '') {
+        $('.widget-facebook').remove();
     }
 }
 
 function dimmer(action, speed) {
-    "use strict";
-    var dimmer = jQuery(".dimmer");
+    'use strict';
+    var dimmer = jQuery('.dimmer');
 
     switch (action) {
-        case "open":
+        case 'open':
             dimmer.fadeIn(speed);
             break;
-        case "close":
+        case 'close':
             dimmer.fadeOut(speed);
             break;
     }
