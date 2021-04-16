1d7e191fc5170920fb6ea8b065c7004f18cf26cd
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2021-04-16 15:48:01 +0800

    Shared gulp watcher and task re-using

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,32 @@ function handleError(done) {
     };
 };
 
+function doCSS(path, done) {
+    pump([
+        src(`${path}/assets/css/screen.css`, {sourcemaps: true}),
+        postcss([
+            easyimport,
+            autoprefixer(),
+            cssnano()
+        ]),
+        dest(`${path}/assets/built/`, {sourcemaps: '.'}),
+        livereload()
+    ], handleError(done));
+}
+
+function doJS(path, done) {
+    pump([
+        src([
+            `${path}/assets/js/lib/*.js`,
+            `${path}/assets/js/main.js`
+        ], {sourcemaps: true}),
+        concat('main.min.js'),
+        uglify(),
+        dest(`${path}/assets/built/`, {sourcemaps: '.'}),
+        livereload()
+    ], handleError(done));
+}
+
 function main(done) {
     const tasks = glob.sync('packages/*').map(path => {
         const packageName = path.replace('packages/', '');
@@ -43,51 +69,17 @@ function main(done) {
             hbs.displayName = `hbs_${packageName}`;
 
             function css(done) {
-                pump([
-                    src(`${path}/assets/css/screen.css`, {sourcemaps: true}),
-                    postcss([
-                        easyimport,
-                        autoprefixer(),
-                        cssnano()
-                    ]),
-                    dest(`${path}/assets/built/`, {sourcemaps: '.'}),
-                    livereload()
-                ], handleError(done));
+                doCSS(path, done);
             }
             css.displayName = `css_${packageName}`;
 
             function js(done) {
-                pump([
-                    src([
-                        `${path}/assets/js/lib/*.js`,
-                        `${path}/assets/js/main.js`
-                    ], {sourcemaps: true}),
-                    concat('main.min.js'),
-                    uglify(),
-                    dest(`${path}/assets/built/`, {sourcemaps: '.'}),
-                    livereload()
-                ], handleError(done));
+                doJS(path, done);
             }
             js.displayName = `js_${packageName}`;
 
-            function allCSS(done) {
-                pump([
-                    src(`packages/*/assets/css/screen.css`, {sourcemaps: true}),
-                    postcss([
-                        easyimport,
-                        autoprefixer(),
-                        cssnano()
-                    ]),
-                    rename(function (path) {
-                        path.dirname = path.dirname.replace('css', 'built');
-                    }),
-                    dest('packages/', {sourcemaps: '.'}),
-                    livereload()
-                ], handleError(done));
-            }
-
             const hbsWatcher = () => watch([`${path}/*.hbs`, `${path}/partials/**/*.hbs`], hbs);
-            const cssWatcher = () => watch(`${path}/assets/css/**/*.css`, packageName === '_shared' ? allCSS : css);
+            const cssWatcher = () => watch(`${path}/assets/css/**/*.css`, css);
             const jsWatcher = () => watch(`${path}/assets/js/**/*.js`, js);
             const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher);
             const build = series(css, js);
@@ -100,7 +92,26 @@ function main(done) {
         return package;
     });
 
-    return series(...tasks, parallelDone => {
+    function sharedCSS(done) {
+        pump([
+            src(`packages/*/assets/css/screen.css`, {sourcemaps: true}),
+            postcss([
+                easyimport,
+                autoprefixer(),
+                cssnano()
+            ]),
+            rename(function (path) {
+                path.dirname = path.dirname.replace('css', 'built');
+            }),
+            dest('packages/', {sourcemaps: '.'}),
+            livereload()
+        ], handleError(done));
+    }
+
+    const sharedCSSWatcher = () => watch('packages/_shared/assets/css/**/*.css', sharedCSS);
+    const sharedWatcher = parallel(sharedCSSWatcher);
+
+    return parallel(...tasks, sharedWatcher, parallelDone => {
         parallelDone();
         done();
     })();
