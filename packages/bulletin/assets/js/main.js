7a00056e0fb2d0039394d53174436f2d7b53d43d
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2021-06-17 20:53:49 +0800

    Single post template

--- packages/bulletin/assets/js/main.js
+++ packages/bulletin/assets/js/main.js
@@ -1,3 +1,20 @@
 (function () {
-    pagination(false);
+    var bodyClass = document.body.classList;
+    var overlay = document.querySelector('.gh-modal-overlay');
+
+    document.querySelectorAll('.js-archive').forEach(function (element) {
+        element.addEventListener('click', function () {
+            overlay.style.display = 'block';
+            window.setTimeout(function () {
+                bodyClass.add('is-modal-open');
+            }, 0);
+        })
+    });
+
+    document.querySelector('.gh-modal-close').addEventListener('click', function () {
+        bodyClass.remove('is-modal-open');
+        window.setTimeout(function () {
+            overlay.style.display = 'none';
+        }, 200);
+    });
 })();
