7e8444f1eeef8457f6bf63a5eacc1b40cbe14525
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2021-07-13 11:56:22 +0800

    Fix 100vh issue on mobile

--- packages/bulletin/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? parseInt(result[1], 16) + ',' + parseInt(result[2], 16) + ',' + parseInt(result[3], 16) : null;
-}
-
-var accentColor = getComputedStyle(document.documentElement).getPropertyValue('--ghost-accent-color');
-
-document.documentElement.style.setProperty('--ghost-accent-color-rgb', hexToRgb(accentColor.replace(' ', '')));
