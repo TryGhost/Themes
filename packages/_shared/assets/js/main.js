b38c66376bca442e882247c3953a5f8ef525d462
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2022-02-10 16:37:15 -0500

    Restrict lightbox to imgs with width & height attr

--- packages/_shared/assets/js/main.js
+++ packages/_shared/assets/js/main.js
@@ -1,20 +1,22 @@
 /* Mobile menu burger toggle */
 (function () {
-    var burger = document.querySelector('.gh-burger');
+    var burger = document.querySelector(".gh-burger");
     if (!burger) return;
 
-    burger.addEventListener('click', function () {
-        if (!document.body.classList.contains('is-head-open')) {
-            document.body.classList.add('is-head-open');
+    burger.addEventListener("click", function () {
+        if (!document.body.classList.contains("is-head-open")) {
+            document.body.classList.add("is-head-open");
         } else {
-            document.body.classList.remove('is-head-open');
+            document.body.classList.remove("is-head-open");
         }
     });
 })();
 
 /* Add lightbox to gallery images. */
 (function () {
-    lightbox('.kg-image-card > .kg-image, .kg-gallery-image img');
+    lightbox(
+        ".kg-image-card > .kg-image[width][height], .kg-gallery-image img"
+    );
 })();
 
 /* Responsive video in post content */
@@ -24,8 +26,8 @@
         '.gh-content iframe[src*="youtube-nocookie.com"]',
         '.gh-content iframe[src*="player.vimeo.com"]',
         '.gh-content iframe[src*="kickstarter.com"][src*="video.html"]',
-        '.gh-content object',
-        '.gh-content embed',
+        ".gh-content object",
+        ".gh-content embed",
     ];
-    reframe(document.querySelectorAll(sources.join(',')));
+    reframe(document.querySelectorAll(sources.join(",")));
 })();
