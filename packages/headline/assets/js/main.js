d872b74101923301a4b9bc9b64f82ad69fc808e9
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-03-15 17:53:02 +0800

    Transparent header style for full feature image post

--- packages/headline/assets/js/main.js
+++ packages/headline/assets/js/main.js
@@ -1,3 +1,12 @@
 (function () {
     pagination(true);
 })();
+
+(function () {
+    if (!document.body.classList.contains('post-template')) return;
+
+    const cover = document.querySelector('.gh-cover');
+    const image = cover.querySelector('.gh-cover-image');
+
+    cover.style.setProperty('--cover-height', image.clientWidth * image.naturalHeight / image.naturalWidth + 'px');
+})();
