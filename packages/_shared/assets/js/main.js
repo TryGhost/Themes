8ec29600d36da3779c74ac86edd7e45e1120035e
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-05-30 12:22:26 +0800

    Exclude linked gallery images from lightbox behavior

--- packages/_shared/assets/js/main.js
+++ packages/_shared/assets/js/main.js
@@ -15,7 +15,7 @@
 /* Add lightbox to gallery images. */
 (function () {
     lightbox(
-        '.kg-image-card > .kg-image[width][height], .kg-gallery-image img'
+        '.kg-image-card > .kg-image[width][height], .kg-gallery-image > img'
     );
 })();
 
