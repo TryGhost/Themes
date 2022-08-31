0584df95765a25754e3b36c6d09d36e94f9ea528
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-08-31 21:28:34 +0800

    Fix bugs

--- themes/solo/assets/js/main.js
+++ themes/solo/assets/js/main.js
@@ -12,3 +12,18 @@
         document.body.classList.toggle('comments-opened');
     });
 })();
+
+(function () {
+    const element = document.querySelector('.gh-article-excerpt');
+    if (!element) return;
+
+    let text = element.textContent;
+    const emojiRE = /\p{EPres}|\p{ExtPict}/gu;
+    const emojis = text.match(emojiRE);
+
+    emojis.forEach(function (emoji) {
+        text = text.replace(emoji, `<span class="emoji">${emoji}</span>`);
+    });
+
+    element.innerHTML = text;
+})();
