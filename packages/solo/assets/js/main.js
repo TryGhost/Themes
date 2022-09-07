b46696d790a1b41e660a5e3ac9c8e11692e3fe60
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-09-07 15:11:42 +0800

    Solo theme

---
+++ packages/solo/assets/js/main.js
@@ -0,0 +1,39 @@
+function initParallax() {
+    jarallax(document.querySelectorAll('.has-parallax-feed .gh-card'), {
+        speed: 0.8,
+    });
+}
+
+(function () {
+    initParallax();
+})();
+
+(function () {
+    const toggle = document.querySelector('[data-toggle-comments]');
+    if (!toggle) return;
+
+    toggle.addEventListener('click', function () {
+        document.body.classList.toggle('comments-opened');
+    });
+})();
+
+(function () {
+    const element = document.querySelector('.gh-article-excerpt');
+    if (!element) return;
+
+    let text = element.textContent;
+    const emojiRE = /\p{EPres}|\p{ExtPict}/gu;
+
+    const emojis = text.match(emojiRE);
+    if (!emojis) return;
+
+    emojis.forEach(function (emoji) {
+        text = text.replace(emoji, `<span class="emoji">${emoji}</span>`);
+    });
+
+    element.innerHTML = text;
+})();
+
+(function () {
+    pagination(true, initParallax);
+})();
