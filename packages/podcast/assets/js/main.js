bd64d261b496d14315203d35dac2218f89280e5c
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-12-02 11:09:51 +0800

    Added container classes to archive header components

--- packages/podcast/assets/js/main.js
+++ packages/podcast/assets/js/main.js
@@ -1,6 +1,7 @@
 (function () {
     const header = document.querySelector('.gh-header');
     const services = document.querySelector('.gh-podcast-service-list');
+    if (!header || !services) return;
 
     header.style.setProperty('--header-height', `calc(100vh - ${services.offsetHeight}px)`);
 })();
