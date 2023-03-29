499d7f4891cf9f94593ec0d182055edf98a0abc0
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-03-29 19:23:22 +0800

    Recent feed
    refs https://github.com/TryGhost/Team/issues/2827

--- packages/new-theme/assets/js/main.js
+++ packages/new-theme/assets/js/main.js
@@ -1,3 +1,18 @@
 (function () {
     pagination(true);
 })();
+
+(function () {
+    var observer = new IntersectionObserver(function (entries) {
+        if (!entries[0].isIntersecting) {
+            document.body.classList.add('has-sticky-cta');
+        }
+        else {
+            document.body.classList.remove('has-sticky-cta');
+        }
+    }, {
+        threshold: 0.5
+    });
+
+    observer.observe(document.querySelector('.gh-main > .gh-cta'));
+})();
