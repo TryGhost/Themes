5a9a5de6119695f2a0ba6dc27293bee37ef3e148
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-06-29 08:47:18 +0800

    Added nested list support

--- packages/_shared/assets/js/v2/main.js
+++ packages/_shared/assets/js/v2/main.js
@@ -37,3 +37,14 @@
 (function () {
     dropdown();
 })();
+
+(function () {
+    const content = document.querySelector('.gh-content');
+    if (!content) return;
+
+    content.querySelectorAll('ul li, ol li').forEach(function (item) {
+        if (item.querySelector('ul, ol')) {
+            item.classList.add('kg-nested-list-item');
+        }
+    });
+})();
