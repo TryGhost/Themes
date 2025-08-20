8489e63eab275cf7a0f27524e45fa479f5347937
George M <thebigcheesymeal@gmail.com>
2025-08-21 00:10:41 +0100

    fix(gulp): use default export for gulp-zip in multiple gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ const postcss = require('gulp-postcss');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const beeper = require('beeper');
-const zip = require('gulp-zip');
+const zip = require('gulp-zip').default;
 
 // postcss plugins
 const easyimport = require('postcss-easy-import');
